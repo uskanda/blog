1b248c64b91811f690466c15d9371d6971e5e900
Yusuke Kanda <uskanda@gmail.com>
2013-12-06 16:40:35 +0900

    enable paginate in cycle

--- config.rb
+++ config.rb
@@ -59,9 +59,9 @@ activate :blog do |blog|
   blog.calendar_template = "calendar.html"
 
   # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
 end
 page "/cycle/*", layout: :cycle
 page "/cycle/articles/*", layout: :cycle_article
